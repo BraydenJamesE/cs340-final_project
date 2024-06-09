a829a4dd5a6958e3d781520da2327e7d4e017a3d
Ben Sihavong <sihavobe@oregonstate.edu>
2024-06-08 20:16:25 -0700

    removed ID var and changed password field.

--- createUsers.php
+++ createUsers.php
@@ -2,56 +2,54 @@
 // Include config file
 require_once "config.php";
 
-$Uid = $Uname = $Uemail = $Upass = "";
+$Uname = $Uemail = $Upass = "";
 $Uname_err = $Uemail_err = $Upass_err = "";
 
+// Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    // Validate First name
+    // Validate Name
     $Uname = trim($_POST["Uname"]);
     if(empty($Uname)){
         $Uname_err = "Please enter your name.";
-    } elseif(!filter_var($Uname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $Uname_err = "Please enter a valid Uname.";
+    } elseif(!preg_match("/^[a-zA-Z\s]+$/", $Uname)){
+        $Uname_err = "Please enter a valid name.";
     } 
-    // Validate Last name
+
+    // Validate Email
     $Uemail = trim($_POST["Uemail"]);
     if(empty($Uemail)){
         $Uemail_err = "Please enter an email.";
     } elseif(!filter_var($Uemail, FILTER_VALIDATE_EMAIL)){
-        $Uemail_err = "Please enter a valid email.";
-    } 
+        $Uemail_err = "Please enter a valid email address.";
+    }
 
-	// Validate Uemail
+    // Validate Password
     $Upass = trim($_POST["Upass"]);
     if(empty($Upass)){
         $Upass_err = "Please enter a password.";     
     }
 
     // Check input errors before inserting in database
-    if(empty($Ssn_err) && empty($Uemail_err) && empty($Upass_err) && empty($name_err)){
+    if(empty($Uname_err) && empty($Uemail_err) && empty($Upass_err)){
         // Prepare an insert statement
-        $sql = "INSERT INTO EMPLOYEE (Ssn, Uname, Uemail, Upass) 
-		        VALUES (?, ?, ?, ?)";
+        $sql = "INSERT INTO EMPLOYEE (Uname, Uemail, Upass) VALUES (?, ?, ?)";
          
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "isssdssi", $param_Ssn, $param_Uname, $param_Uemail, 
-				$param_Upass);
+            mysqli_stmt_bind_param($stmt, "sss", $param_Uname, $param_Uemail, $param_Upass);
             
             // Set parameters
-			$param_Ssn = $Ssn;
+            $param_Uname = $Uname;
             $param_Uemail = $Uemail;
-			$param_Uname = $Uname;
-			$param_Upass = $Upass;
+            $param_Upass = password_hash($Upass, PASSWORD_DEFAULT); // Creates a password hash
             
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Records created successfully. Redirect to landing page
-				    header("location: index.php");
-					exit();
+                header("location: index.php");
+                exit();
             } else{
                 echo "<center><h4>Error while creating new employee</h4></center>";
-				$Ssn_err = "Enter a unique Ssn.";
             }
         }
          
@@ -62,9 +60,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Close connection
     mysqli_close($link);
 }
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -89,19 +85,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     </div>
                     <p>Please fill this form and confirm to create a user.</p>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-						<div class="form-group <?php echo (!empty($Uname_err)) ? 'has-error' : ''; ?>">
+                        <div class="form-group <?php echo (!empty($Uname_err)) ? 'has-error' : ''; ?>">
                             <label>Name</label>
                             <input type="text" name="Uname" class="form-control" value="<?php echo $Uname; ?>">
                             <span class="help-block"><?php echo $Uname_err;?></span>
                         </div>
-						<div class="form-group <?php echo (!empty($Uemail_err)) ? 'has-error' : ''; ?>">
+                        <div class="form-group <?php echo (!empty($Uemail_err)) ? 'has-error' : ''; ?>">
                             <label>Email</label>
                             <input type="text" name="Uemail" class="form-control" value="<?php echo $Uemail; ?>">
                             <span class="help-block"><?php echo $Uemail_err;?></span>
                         </div>
                         <div class="form-group <?php echo (!empty($Upass_err)) ? 'has-error' : ''; ?>">
                             <label>Password</label>
-                            <input type="text" name="Upass" class="form-control" value="<?php echo $Upass; ?>">
+                            <input type="password" name="Upass" class="form-control" value="<?php echo $Upass; ?>">
                             <span class="help-block"><?php echo $Upass_err;?></span>
                         </div>
                         <input type="submit" class="btn btn-primary" value="Register">
