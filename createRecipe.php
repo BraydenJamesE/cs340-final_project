27c96976c9e3c0ab6fa637adcfd154f7195719cc
Ben Sihavong <sihavobe@oregonstate.edu>
2024-06-09 20:45:23 -0700

    added ingredients functionality

--- createRecipe.php
+++ createRecipe.php
@@ -6,7 +6,17 @@ session_start();
 $member_id = $_SESSION['id'];
 
 $Rname = $Rtime = $Rsize = null;
-$Rname_err = $Rtime_err = $Rsize_err = "";
+$Rname_err = $Rtime_err = $Rsize_err = $ingredients_err = "";
+
+$ingredientOptions = "";
+$sql = "SELECT `ingredient name` FROM ingredient";
+$result = mysqli_query($link, $sql);
+if (!$result) {
+    die("Error fetching ingredients: " . mysqli_error($link));
+}
+while ($row = mysqli_fetch_assoc($result)) {
+    $ingredientOptions .= "<option value='" . htmlspecialchars($row['ingredient name']) . "'>" . htmlspecialchars($row['ingredient name']) . "</option>";
+}
 
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -33,8 +43,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $Rsize_err = "Please enter a valid serving size.";
     }
 
+    $Ringredients = $_POST['ingredients'];
+    if(empty($Ringredients)){
+        $ingredients_err = "Please select at least one ingredient.";
+    }
+
     // Check input errors before inserting in database
-    if(empty($Rtime_err) && empty($Rsize_err) && empty($Rname_err)){
+    if(empty($Rtime_err) && empty($Rsize_err) && empty($Rname_err) && empty($ingredients_err)){
         // Prepare an insert statement
         $sql = "INSERT INTO recipe (`recipe name`, `cook time`, `serving size`, `member id`) VALUES (?, ?, ?, ?)";
          
@@ -48,15 +63,26 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $param_Rsize = $Rsize;
             $param_member_id = $member_id;
 
-
-
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
+
+                $sql_contains = "INSERT INTO contains (`recipe name`, `ingredient name`) VALUES (?, ?)";
+                if($stmt_contains = mysqli_prepare($link, $sql_contains)){
+                    mysqli_stmt_bind_param($stmt_contains, "ss", $param_recipe_name, $param_ingredient_name);
+
+                    foreach($Ringredients as $ingredient_name){
+                        $param_recipe_name = $Rname;
+                        $param_ingredient_name = $ingredient_name;
+                        mysqli_stmt_execute($stmt_contains);
+                    }
+                }
+
+                mysqli_stmt_close($stmt_contains);
                 // Records created successfully. Redirect to landing page
                 header("location: index.php");
                 exit();
             } else{
-                echo "<center><h4>Error while creating new user</h4></center>";
+                echo "<center><h4>Error while creating new recipe</h4></center>";
             }
         }
          
@@ -106,6 +132,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             <label>Serving Size</label>
                             <input type="text" name="Rsize" class="form-control" value="<?php echo $Rsize; ?>">
                             <span class="help-block"><?php echo $Rsize_err;?></span>
+                        </div>
+                        <div class="form-group <?php echo (!empty($ingredients_err)) ? 'has-error' : ''; ?>">
+                            <label>Ingredients</label>
+                            <select name="ingredients[]" class="form-control" multiple>
+                                <?php echo $ingredientOptions; ?>
+                            </select>
+                            <span class="help-block"><?php echo $ingredients_err;?></span>
                         </div>
                         <input type="submit" class="btn btn-primary" value="Add">
                         <a href="index.php" class="btn btn-default">Cancel</a>
